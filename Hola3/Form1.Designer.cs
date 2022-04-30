10cd0e4667faa46eede55213bdeb252527295d4c
Jesús Castro García <jesus062192@gmail.com>
2022-04-29 20:21:52 -0500

    ola

--- Hola3/Form1.Designer.cs
+++ Hola3/Form1.Designer.cs
@@ -31,15 +31,16 @@
             this.Bienvenido = new System.Windows.Forms.Label();
             this.BtnCot = new System.Windows.Forms.Button();
             this.BtnReg = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Bienvenido
             // 
             this.Bienvenido.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.Bienvenido.Font = new System.Drawing.Font("Sitka Heading", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.Bienvenido.Location = new System.Drawing.Point(246, 41);
+            this.Bienvenido.Location = new System.Drawing.Point(273, 111);
             this.Bienvenido.Name = "Bienvenido";
-            this.Bienvenido.Size = new System.Drawing.Size(356, 110);
+            this.Bienvenido.Size = new System.Drawing.Size(359, 101);
             this.Bienvenido.TabIndex = 0;
             this.Bienvenido.Text = "Bienvenido";
             this.Bienvenido.Click += new System.EventHandler(this.Bienvenido_Click);
@@ -48,7 +49,7 @@
             // 
             this.BtnCot.Cursor = System.Windows.Forms.Cursors.Hand;
             this.BtnCot.Font = new System.Drawing.Font("Sitka Text", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnCot.Location = new System.Drawing.Point(87, 276);
+            this.BtnCot.Location = new System.Drawing.Point(152, 276);
             this.BtnCot.Name = "BtnCot";
             this.BtnCot.Size = new System.Drawing.Size(177, 97);
             this.BtnCot.TabIndex = 1;
@@ -60,19 +61,31 @@
             // 
             this.BtnReg.Cursor = System.Windows.Forms.Cursors.Hand;
             this.BtnReg.Font = new System.Drawing.Font("Sitka Text", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnReg.Location = new System.Drawing.Point(599, 276);
+            this.BtnReg.Location = new System.Drawing.Point(348, 276);
             this.BtnReg.Name = "BtnReg";
-            this.BtnReg.Size = new System.Drawing.Size(186, 97);
+            this.BtnReg.Size = new System.Drawing.Size(175, 97);
             this.BtnReg.TabIndex = 2;
             this.BtnReg.Text = "Empleado/Empresa";
             this.BtnReg.UseVisualStyleBackColor = true;
             this.BtnReg.Click += new System.EventHandler(this.BtnReg_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Sitka Text", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(556, 276);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 97);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Empleado/Empresa";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(863, 487);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.BtnReg);
             this.Controls.Add(this.BtnCot);
             this.Controls.Add(this.Bienvenido);
@@ -88,5 +101,6 @@
         private Label Bienvenido;
         private Button BtnCot;
         private Button BtnReg;
+        private Button button3;
     }
 }
